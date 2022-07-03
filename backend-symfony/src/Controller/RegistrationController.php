680aba0c551ed33c5280d22fa3d898adb690961c
ujikstark <ujikbauk@gmail.com>
2022-07-03 17:15:31 +0700

    created registration controller

---
+++ backend-symfony/src/Controller/RegistrationController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Annotation\Route;
+
+class RegistrationController extends AbstractController
+{
+    public function __construct(
+        private UserRepository $userRepository,
+        private UserPasswordHasherInterface $hasher
+    ) {   
+    }
+
+
+
+    #[Route('/registration', name: 'app_registration')]
+    public function index(Request $request)
+    {
+        $input = json_decode($request->getContent(), true);
+
+        $user = new User();
+
+        $userId = str_split($input['username']);
+
+        if (count($userId) != 5 || (int)$input['username'] == 0) return $this->json('error', 400);
+        (int)$sum1 = (int)$userId[0]+(int)$userId[1]+(int)$userId[2];
+        (int)$sum2 = $userId[3].$userId[4];
+        if ($sum1 != $sum2) return $this->json('error', 400);
+
+
+        $user->setUsername($input['username']);
+        $plaintextPassword = $input['password'];
+
+        $hashedPassword = $this->hasher->hashPassword($user, $plaintextPassword);
+
+        $user->setPassword($hashedPassword);
+
+        $this->userRepository->save($user);
+
+        return $this->json([
+            'success register' 
+        ]);
+    }
+}
--- backend-symfony/src/Entity/User.php
+++ backend-symfony/src/Entity/User.php
@@ -15,7 +15,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Column(type: 'integer')]
     private $id;
 
-    #[ORM\Column(type: 'string', length: 180, unique: true)]
+    #[ORM\Column(type: 'string', length: 5)]
     private $username;
 
     #[ORM\Column(type: 'json')]
