dfd9c1130cdceac0eb97354fc32c24ed5633a671
ujikstark <ujikbauk@gmail.com>
2022-07-03 17:26:07 +0700

    create login controller

---
+++ backend-symfony/src/Controller/LoginController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class LoginController extends AbstractController
+{
+    #[Route('/login', name: 'app_login')]
+    public function index(): JsonResponse
+    {
+        $user = $this->getUser();
+
+        if (null === $user) {
+            return $this->json([
+                'message' => 'missing credentials',
+            ], Response::HTTP_UNAUTHORIZED);
+        }
+
+        return $this->json([
+            'user'  => $user->getUserIdentifier(),
+        ]);
+
+
+    }
+}
